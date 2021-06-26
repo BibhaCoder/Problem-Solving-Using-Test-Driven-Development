2f8c2a412a1789eeec0a29ca80de83f2d05f04f5
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2021-06-26 11:32:27 -0700

    Create lru_cache.h

---
+++ lru_cache/inc/lru_cache.h
@@ -0,0 +1,19 @@
+#include <stdint.h>
+#include <stddef.h>
+
+struct lru_cache {
+	size_t key;
+	int32_t value;
+	struct lru_cache_hit_list *node;
+};
+
+#define MAX_ITEM_KEY (20)
+
+#define MAX_ITEMS_IN_LRU_CACHE  (10)
+
+int add_item(size_t key, int32_t value);
+struct lru_cache *find_item(size_t key);
+struct lru_cache *get_lru_cache_most_recent_hit(void);
+int remove_item(size_t key);
+void print_lru_cache(void);
+int destroy_lru_cache(void);

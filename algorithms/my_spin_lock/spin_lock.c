ac471192826be0d031c05b285e7e92c7dbd3d532
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2021-07-09 17:14:24 -0700

    Update spin_lock.c

--- algorithms/my_spin_lock/spin_lock.c
+++ algorithms/my_spin_lock/spin_lock.c
@@ -7,10 +7,12 @@ static volatile uint32_t my_spin_lock = 0;
 
 void spin_lock(void)
 {
-	/* full memory barrier to avoid cache coherency issues on multi core systems */
 	while (__sync_lock_test_and_set(&my_spin_lock, 1)) 
 		printf("keep spining as spin lock is not available\n");
 
+	/* full memory barrier to avoid cache coherency issues on multi core systems */
+	__sync_synchronize();
+	
 	printf("stop spinning as spin lock is available and acquire it\n");
 }
 
@@ -18,7 +20,9 @@ void spin_unlock(void)
 {
 	/* full memory barrier to avoid cache coherency issues on multi core systems */
 	__sync_synchronize();
+	
 	my_spin_lock = 0;
+	
 	printf("release spin lock\n");
 }
 

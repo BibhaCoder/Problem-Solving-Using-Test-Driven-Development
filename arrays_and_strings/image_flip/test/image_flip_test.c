8f925ca5476db38ee085231f67cf898ffe9d3784
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2021-07-18 10:22:10 -0700

    Update image_flip_test.c

--- arrays_and_strings/image_flip/test/image_flip_test.c
+++ arrays_and_strings/image_flip/test/image_flip_test.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2017 BibhaCoder(https://github.com/BibhaCoder). All rights reserved.
+ *
+ * Licensed under the MIT License. See MIT license for full license information.
+ */
+
 #include "../inc/image_flip.h"
 #include "/Unity/src/unity.h"
 

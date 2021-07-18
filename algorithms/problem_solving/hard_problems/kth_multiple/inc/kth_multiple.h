6a28d8af31b795b7a3cc4f7c4b17ad6d726b0a50
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2021-07-17 22:12:33 -0700

    Update kth_multiple.h

--- algorithms/problem_solving/hard_problems/kth_multiple/inc/kth_multiple.h
+++ algorithms/problem_solving/hard_problems/kth_multiple/inc/kth_multiple.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2017 BibhaCoder(https://github.com/BibhaCoder). All rights reserved.
+ *
+ * Licensed under the MIT License. See MIT license for full license information.
+ */
+
 #define MAX_NUMBER (10000)
 
 size_t kth_multiple(size_t k);

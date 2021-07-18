3b827b144e77b0b0067038d54fbe78ae66552539
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2021-07-17 22:15:02 -0700

    Update rain_water_tap.h

--- algorithms/rain_water_tapping/inc/rain_water_tap.h
+++ algorithms/rain_water_tapping/inc/rain_water_tap.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2017 BibhaCoder(https://github.com/BibhaCoder). All rights reserved.
+ *
+ * Licensed under the MIT License. See MIT license for full license information.
+ */
+
 #include <stddef.h>
 
 size_t tap_rain_water(size_t const *arr, size_t array_size);

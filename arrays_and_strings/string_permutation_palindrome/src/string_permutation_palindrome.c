683b62e8abc02613e683c9f5561cbd7ddd93f09b
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2021-07-18 15:42:38 -0700

    Update string_permutation_palindrome.c

--- arrays_and_strings/string_permutation_palindrome/src/string_permutation_palindrome.c
+++ arrays_and_strings/string_permutation_palindrome/src/string_permutation_palindrome.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2017 BibhaCoder(https://github.com/BibhaCoder). All rights reserved.
+ *
+ * Licensed under the MIT License. See MIT license for full license information.
+ */
+
 #include <string.h>
 #include <ctype.h>
 #include <stdint.h>

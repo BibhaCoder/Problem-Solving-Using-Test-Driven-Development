e7d62971ebcb19b34a69a59a3e9882e2ad571c06
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2021-07-17 22:00:22 -0700

    Update graph_search_dfs.c

--- algorithms/graph_search_using_dfs/src/graph_search_dfs.c
+++ algorithms/graph_search_using_dfs/src/graph_search_dfs.c
@@ -1,3 +1,8 @@
+/*  
+ * Copyright (c) 2017 BibhaCoder(bibha.rajeev.2011@gmail.com). All rights reserved.  
+ * Licensed under the MIT License. See MIT license for full license information.  
+ */
+
 #include <stdlib.h>
 #include <stdbool.h>
 #include "../inc/graph_search_dfs.h"

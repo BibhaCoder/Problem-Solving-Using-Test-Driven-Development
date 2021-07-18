2b5a38e7582c428acb1bac0813d9e27cc74a217c
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2021-07-18 15:24:32 -0700

    Update is_perm_palindrome.c

--- arrays_and_strings/is_permutation_palindrome/src/is_perm_palindrome.c
+++ arrays_and_strings/is_permutation_palindrome/src/is_perm_palindrome.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2017 BibhaCoder(https://github.com/BibhaCoder). All rights reserved.
+ *
+ * Licensed under the MIT License. See MIT license for full license information.
+ */
+
 #include "../inc/is_perm_palindrome.h"
 #include <stddef.h>
 #include <stdint.h>

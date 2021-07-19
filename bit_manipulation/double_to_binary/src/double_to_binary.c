62b4ad47fc6fabbbbc39bd7a0863c51e942be53a
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2021-07-19 10:14:41 -0700

    Update double_to_binary.c

--- bit_manipulation/double_to_binary/src/double_to_binary.c
+++ bit_manipulation/double_to_binary/src/double_to_binary.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2017 BibhaCoder(https://github.com/BibhaCoder). All rights reserved.
+ *
+ * Licensed under the MIT License. See MIT license for full license information.
+ */
+
 #include "../inc/double_to_binary.h"
 #include <stdbool.h>
 #include <stdlib.h>

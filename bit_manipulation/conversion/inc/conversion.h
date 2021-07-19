559d456388dee0d66e7a09b744119da04ebc7d75
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2021-07-19 10:12:43 -0700

    Update conversion.h

--- bit_manipulation/conversion/inc/conversion.h
+++ bit_manipulation/conversion/inc/conversion.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2017 BibhaCoder(https://github.com/BibhaCoder). All rights reserved.
+ *
+ * Licensed under the MIT License. See MIT license for full license information.
+ */
+
 #include <stdint.h>
 
 uint8_t conversion(uint32_t num1, uint32_t num2);

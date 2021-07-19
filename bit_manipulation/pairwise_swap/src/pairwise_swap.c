a94b6ab749f10a771fe323e90840ce54bcdfcd72
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2021-07-19 10:18:32 -0700

    Update pairwise_swap.c

--- bit_manipulation/pairwise_swap/src/pairwise_swap.c
+++ bit_manipulation/pairwise_swap/src/pairwise_swap.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2017 BibhaCoder(https://github.com/BibhaCoder). All rights reserved.
+ *
+ * Licensed under the MIT License. See MIT license for full license information.
+ */
+
 #include "../inc/pairwise_swap.h"
 
 uint32_t pairwise_swap(uint32_t val)

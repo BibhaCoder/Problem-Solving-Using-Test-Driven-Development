da37317f7eabbb2c337ad169686722d1ae8f07c5
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2021-07-19 10:19:47 -0700

    Update test_rightrot.c

--- bit_manipulation/rightrot/test/test_rightrot.c
+++ bit_manipulation/rightrot/test/test_rightrot.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2017 BibhaCoder(https://github.com/BibhaCoder). All rights reserved.
+ *
+ * Licensed under the MIT License. See MIT license for full license information.
+ */
+
 #include </Unity/src/unity.h>
 #include <stdint.h>
 #include "../inc/rightrot.h"

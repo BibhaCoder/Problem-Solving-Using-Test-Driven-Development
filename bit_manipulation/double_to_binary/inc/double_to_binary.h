f77dcaa191645f25edd66ba5b0ba965f4eae7819
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2021-07-19 10:14:24 -0700

    Update double_to_binary.h

--- bit_manipulation/double_to_binary/inc/double_to_binary.h
+++ bit_manipulation/double_to_binary/inc/double_to_binary.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2017 BibhaCoder(https://github.com/BibhaCoder). All rights reserved.
+ *
+ * Licensed under the MIT License. See MIT license for full license information.
+ */
+
 #define MAX_BINARY_SIZE (32)
 
 char *double_to_binary(double val);

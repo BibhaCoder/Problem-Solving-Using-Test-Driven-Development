a07b2fee12dedf86c71ae62fab926a6886fabe4c
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2021-07-19 10:21:07 -0700

    Update squeeze.h

--- bit_manipulation/squeeze/inc/squeeze.h
+++ bit_manipulation/squeeze/inc/squeeze.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2017 BibhaCoder(https://github.com/BibhaCoder). All rights reserved.
+ *
+ * Licensed under the MIT License. See MIT license for full license information.
+ */
+
 #define MAX_INPUT_LINE_SIZE (100)
 
 int squeeze(char *s, char c);

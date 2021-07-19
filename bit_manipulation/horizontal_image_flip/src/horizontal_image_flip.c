45d8007ba61c7d215e99236c962c64fbf29ac518
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2021-07-19 10:16:34 -0700

    Update horizontal_image_flip.c

--- bit_manipulation/horizontal_image_flip/src/horizontal_image_flip.c
+++ bit_manipulation/horizontal_image_flip/src/horizontal_image_flip.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2017 BibhaCoder(https://github.com/BibhaCoder). All rights reserved.
+ *
+ * Licensed under the MIT License. See MIT license for full license information.
+ */
+
 #include "../inc/horizontal_image_flip.h"
 #include <stdbool.h>
 #include <limits.h>

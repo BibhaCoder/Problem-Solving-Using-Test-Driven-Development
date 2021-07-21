a30f9bb4ad2149b6c3f0d6a07b4d279ce2eb02c8
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2021-07-21 10:29:56 -0700

    Update graph_search_bfs.c

--- graph/graph_search_using_bfs/src/graph_search_bfs.c
+++ graph/graph_search_using_bfs/src/graph_search_bfs.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2017 BibhaCoder(https://github.com/BibhaCoder). All rights reserved.
+ *
+ * Licensed under the MIT License. See MIT license for full license information.
+ */
+
 #include <stdlib.h>
 #include <stdbool.h>
 #include "../inc/graph_search_bfs.h"

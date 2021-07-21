b20a296e39de1bcac0c51319a796dcf8e877b623
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2021-07-21 14:35:20 -0700

    Update string_reverse.h

--- knr_systems_software/chapter_1/string_reverse/inc/string_reverse.h
+++ knr_systems_software/chapter_1/string_reverse/inc/string_reverse.h
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2017 BibhaCoder(https://github.com/BibhaCoder). All rights reserved.
+ *
+ * Licensed under the MIT License. See MIT license for full license information.
+ */
 
 char *string_reverse(char const *s);
 

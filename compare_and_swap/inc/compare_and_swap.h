8a210a6b0570a5edee2a98656f06bbdaedcafe86
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2021-07-21 10:20:54 -0700

    Update compare_and_swap.h

--- compare_and_swap/inc/compare_and_swap.h
+++ compare_and_swap/inc/compare_and_swap.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2017 BibhaCoder(https://github.com/BibhaCoder). All rights reserved.
+ *
+ * Licensed under the MIT License. See MIT license for full license information.
+ */
+
 #include <stdint.h>
 
 int32_t compare_and_swap(int32_t *ref, int32_t old_val, int32_t new_val);

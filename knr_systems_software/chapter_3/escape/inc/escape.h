c0e3004ef8dfcf8a01506adae79e52b8451ec882
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2021-07-22 13:59:35 -0700

    Update escape.h

--- knr_systems_software/chapter_3/escape/inc/escape.h
+++ knr_systems_software/chapter_3/escape/inc/escape.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2017 BibhaCoder(https://github.com/BibhaCoder). All rights reserved.
+ *
+ * Licensed under the MIT License. See MIT license for full license information.
+ */
+
 #define MAX_INPUT_SIZE (10)
 
 char const *escape(char const *s);

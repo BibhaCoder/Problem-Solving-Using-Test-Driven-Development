3063cd621cac1652f7ade9f00a56ae7d2b125553
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2021-07-22 14:07:22 -0700

    Update strcmp.c

--- knr_systems_software/chapter_4/strcmp.c
+++ knr_systems_software/chapter_4/strcmp.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2017 BibhaCoder(https://github.com/BibhaCoder). All rights reserved.
+ *
+ * Licensed under the MIT License. See MIT license for full license information.
+ */
+
 #include <stdio.h>
 
 int my_strcmp(char const *s, char const *t)

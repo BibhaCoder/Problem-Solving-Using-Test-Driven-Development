3f6df56144623a430e6793d5e8bfa926d3d9ae9c
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2021-07-22 14:04:20 -0700

    Update strindex.h

--- knr_systems_software/chapter_3/strindex/inc/strindex.h
+++ knr_systems_software/chapter_3/strindex/inc/strindex.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2017 BibhaCoder(https://github.com/BibhaCoder). All rights reserved.
+ *
+ * Licensed under the MIT License. See MIT license for full license information.
+ */
+
 #define MAX_SIZE (20)
 
 ssize_t strindex(char const *s, char const *t);

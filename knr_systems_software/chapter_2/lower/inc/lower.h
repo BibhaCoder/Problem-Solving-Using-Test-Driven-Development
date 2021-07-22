88f41945d8470753180dac33ccfb54365332c4a1
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2021-07-22 13:51:00 -0700

    Update lower.h

--- knr_systems_software/chapter_2/lower/inc/lower.h
+++ knr_systems_software/chapter_2/lower/inc/lower.h
@@ -1,1 +1,7 @@
+/*
+ * Copyright (c) 2017 BibhaCoder(https://github.com/BibhaCoder). All rights reserved.
+ *
+ * Licensed under the MIT License. See MIT license for full license information.
+ */
+
 char lower(char c);

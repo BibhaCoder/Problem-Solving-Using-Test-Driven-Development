c1495305ed45ca29c90d1d8067a3778959784bd6
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2021-07-22 14:01:16 -0700

    Update integer_to_string.h

--- knr_systems_software/chapter_3/itoa/inc/integer_to_string.h
+++ knr_systems_software/chapter_3/itoa/inc/integer_to_string.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2017 BibhaCoder(https://github.com/BibhaCoder). All rights reserved.
+ *
+ * Licensed under the MIT License. See MIT license for full license information.
+ */
+
 #define MAX_SIZE (100)
 
 void integer_to_string(int64_t n, char *s);

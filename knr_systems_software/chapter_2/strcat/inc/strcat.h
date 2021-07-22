e40f3d1548ef030a91976d7bacdd0deb609a5279
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2021-07-22 13:55:32 -0700

    Update strcat.h

--- knr_systems_software/chapter_2/strcat/inc/strcat.h
+++ knr_systems_software/chapter_2/strcat/inc/strcat.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2017 BibhaCoder(https://github.com/BibhaCoder). All rights reserved.
+ *
+ * Licensed under the MIT License. See MIT license for full license information.
+ */
+
 #define MAX_INPUT_LINE_SIZE (40)
 
 int my_strcat(char *s, char const *t);

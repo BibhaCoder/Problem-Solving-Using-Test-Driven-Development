4659862024e9ad8a400fb7ac3b48dff3b5255467
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2021-07-22 14:07:09 -0700

    Update 5-3.c

--- knr_systems_software/chapter_4/5-3.c
+++ knr_systems_software/chapter_4/5-3.c
@@ -1,4 +1,10 @@
-#include <stdio.h>
+/*
+ * Copyright (c) 2017 BibhaCoder(https://github.com/BibhaCoder). All rights reserved.
+ *
+ * Licensed under the MIT License. See MIT license for full license information.
+ */
+
+include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 

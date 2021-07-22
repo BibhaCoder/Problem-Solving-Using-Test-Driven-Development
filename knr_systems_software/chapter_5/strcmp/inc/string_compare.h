1f953efef6234f564223f9c1d8f68ddf0e0d87e2
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2021-07-22 14:08:33 -0700

    Update string_compare.h

--- knr_systems_software/chapter_5/strcmp/inc/string_compare.h
+++ knr_systems_software/chapter_5/strcmp/inc/string_compare.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2017 BibhaCoder(https://github.com/BibhaCoder). All rights reserved.
+ *
+ * Licensed under the MIT License. See MIT license for full license information.
+ */
+
 #define MAX_STRING_SIZE (10)
 
 int string_compare(char const *s, char const *t, ssize_t *diff);

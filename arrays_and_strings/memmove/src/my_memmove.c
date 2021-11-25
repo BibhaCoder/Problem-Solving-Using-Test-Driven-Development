64f2a7cf3b3465c23569f2a1def556ea414dec78
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2021-11-25 15:37:41 -0800

    Update my_memmove.c

--- arrays_and_strings/memmove/src/my_memmove.c
+++ arrays_and_strings/memmove/src/my_memmove.c
@@ -1,7 +1,5 @@
 /*
  * Copyright (c) 2017 BibhaCoder(https://github.com/BibhaCoder). All rights reserved.
- *
- * Licensed under the MIT License. See MIT license for full license information.
  */
 
 #include "../inc/my_memmove.h"

0fb9af4dfd0ca09775e484d215eeac86eaa25d37
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2021-12-03 15:11:30 -0800

    Update integer_reverse.c

--- algorithms/integer_reverse/src/integer_reverse.c
+++ algorithms/integer_reverse/src/integer_reverse.c
@@ -20,17 +20,39 @@ static int32_t __integer_reverse(int32_t num)
 	return out_num;
 }
 
+static bool is_input_valid(int32_t num)
+{
+	int32_t last_digit;
+
+	/* invalid inputs which can lead to int32 overflow */
+
+	if ((num > 1000000000)) {
+		last_digit = num % 10;
+		if (last_digit > 2)
+			return false;
+	}
+
+	if ((num < -1000000000)) {
+		last_digit = num % 10;
+		if (last_digit < -2)
+			return false;
+	}
+
+	if (INT_MIN == num)
+		return false;
+
+	if (INT_MAX == num)
+		return false;
+
+	return true;
+}
+
 int32_t integer_reverse(int32_t num)
 {
 	if ((-10 < num) && (num < 10))
 		return num;
 
-	/* invalid input can lead to int32 overflow */
-	if (INT_MIN == num)
-		return -1;
-
-	/* invalid input can lead to int32 overflow */
-	if (INT_MAX == num)
+	if (!is_input_valid(num))
 		return -1;
 
 	return __integer_reverse(num);

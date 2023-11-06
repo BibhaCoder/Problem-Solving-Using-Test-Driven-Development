fec9d4b63f2a25bcc48ab73db835ce36e5e4ec6f
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2023-11-07 02:06:12 +0530

    Update and rename endianess c to endianess.c

--- endianess/endianess.c
+++ endianess/endianess.c
@@ -2,13 +2,13 @@
 
 int main() 
 {
-        uint32_t word = 0xaabbccdd;
+        uint32_t word = 0x00000001;
         uint8_t *byte = (uint8_t *)&word;
 
-        if (0xaa == (*byte))
-                printf("Big endian");
-        else
+        if (0x1 == (*byte))
                 printf("Little endian");
+        else
+                printf("Big endian");
 
         return 0;
 }

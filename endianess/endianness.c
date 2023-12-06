5bab74831bfad42fcb8ccfab9912ca68684784b2
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2023-12-06 12:58:05 -0800

    Update endianness.c

--- endianess/endianness.c
+++ endianess/endianness.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
+#include <stdint.h>
 
-int main() 
+static void dump_memory(uint8_t *byte)
+{
+        printf("address -> %p : val 0x%x\n", byte, *byte);
+        printf("address -> %p : val 0x%x\n", (byte + 1), *(byte + 1));
+        printf("address -> %p : val 0x%x\n", (byte + 2), *(byte + 2));
+        printf("address -> %p : val 0x%x\n", (byte + 3), *(byte + 3));
+}
+
+int main()
 {
         uint32_t word = 0xaabbccdd;
         uint8_t *byte = (uint8_t *)&word;
 
         if (0xaa == (*byte))
-                printf("Big endian");
+                printf("Big endian lowest address %p and val 0x%x\n", byte, *byte);
         else
-                printf("Little endian");
+                printf("Little endian lowest address %p and val 0x%x\n", byte, *byte);
 
-        return 0;
+        dump_memory(byte);
 }

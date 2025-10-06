cfacd8803dde0042542ecc1f09a3e33411051fe4
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2025-10-06 12:18:53 -0700

    Update compare_and_swap.h

--- compare_and_swap/inc/compare_and_swap.h
+++ compare_and_swap/inc/compare_and_swap.h
@@ -4,4 +4,4 @@
 
 #include <stdint.h>
 
-int32_t compare_and_swap(int32_t *ref, int32_t old_val, int32_t new_val);
+int32_t compare_and_swap(int32_t volatile *ref, int32_t old_val, int32_t new_val);

2a67a3c71665c536ef394d5539fc3ee49f2b7392
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2026-02-14 10:29:08 -0800

    Update single_dma_two_sensors.c

--- algorithms/sensors/single_dma_two_sensors.c
+++ algorithms/sensors/single_dma_two_sensors.c
@@ -1,1 +1,7 @@
-
+/**
+ * 1 single DMA non thread safe API
+ * 2 sensors:
+ * 1) One real-time sensors which has hard real time scheduling requirements (less than 5 ms)
+ * 2) Second non real-time sensor which does not real time scheduling constraints but generates 5 times mote data
+ *
+ */

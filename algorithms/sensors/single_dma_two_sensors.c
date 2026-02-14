6af14e94385c514bc2448b880e355a0057900ec7
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2026-02-14 10:46:07 -0800

    Update single_dma_two_sensors.c

--- algorithms/sensors/single_dma_two_sensors.c
+++ algorithms/sensors/single_dma_two_sensors.c
@@ -5,3 +5,18 @@
  * 2) Second non real-time sensor which does not real time scheduling constraints but generates 5 times mote data
  *
  */
+
+/**
+ * Design options:
+ * 3 threads:
+ * 1) DMA thread: Highest priority DMA threads which moves data written by sensors to 2 separate rings buffers
+ * 2) RT thread: High priority threads which processes real time sensor
+ * 3) Non-RT thread: Low priority thread which processes non real time sensor on best effort basis.
+ *
+ * Only 1 interrupt from both thr sensors to avoid interrupt strom.
+ * All 3 threads are bottom halves.
+ * Highest priority DMA thread is the only caller of non-thread safe DMA API and copies sensors data to their respective ring buffers and schedule both RT and non-RT thread to run.
+ * RT thread processes RT ring buffers and meets scheduling KPIs.
+ * Non RT thread does best effort processing of non RT ring buffers.
+ *
+ */

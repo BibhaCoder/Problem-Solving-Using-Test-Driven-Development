b3a4e842243e5fea9b926becf39bfbe0a402eaa8
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2026-02-27 13:13:13 -0800

    Update ring_buffer_no_wastage.c

--- arrays_and_strings/ring_buffer/src/ring_buffer_no_wastage.c
+++ arrays_and_strings/ring_buffer/src/ring_buffer_no_wastage.c
@@ -9,6 +9,7 @@
 static int ring_buffer[MAX_RING_BUFFER_SIZE];
 static size_t read_pointer = 0;
 static size_t write_pointer = 1;
+static size_t count = 0;
 
 static void increment_pointer(size_t *pointer)
 {
@@ -18,22 +19,24 @@ static void increment_pointer(size_t *pointer)
 
 int dequeue(int *out_val)
 {
-	if (((read_pointer + 1) & (MAX_RING_BUFFER_SIZE - 1)) == write_pointer)
+	if (!count)
 		return -1;
 
 	increment_pointer(&read_pointer);
 	*out_val = ring_buffer[read_pointer];
+	count--;
 
 	return 0;
 }
 
 int enqueue(int val)
 {
-	if (write_pointer == read_pointer)
+	if (MAX_RING_BUFFER_SIZE == count)
 		return -1;
 
 	ring_buffer[write_pointer] = val;
 	increment_pointer(&write_pointer);
+	count++;
 
 	return 0;
 }
